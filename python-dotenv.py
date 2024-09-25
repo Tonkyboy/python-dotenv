7d9c01e13e50b46ea2de90e528f3624ef0bc6294
Alexander Reute <68352880+Tonkyboy@users.noreply.github.com>
2024-09-25 15:43:49 +0200

    Update python-dotenv.py

--- python-dotenv.py
+++ python-dotenv.py
@@ -14,18 +14,14 @@ load_dotenv('.secrets.env')
 # add .env to directory
 # add your .env to .gitignore
 
-# Development settings
-DOMAIN=example.org
-# If you use variables in values, ensure they are surrounded with { and }, like ${DOMAIN}
-ADMIN_EMAIL=admin@${DOMAIN}
-ROOT_URL=${DOMAIN}/app
-
 # Read the environment variables
 import os
 domain_getenv = os.getenv("DOMAIN")
 # or
 domain_environ = os.environ["DOMAIN"]
-#In this case, both lines are used to access the value of the "DOMAIN" environment variable, but the first line returnsNoneif the variable is not found, while the second line raises aKeyErrorif the variable is not present in the environment.
+#In this case, both lines are used to access the value of the "DOMAIN" environment variable, 
+#but the first line returnsNoneif the variable is not found, while the second line raises a
+#KeyError if the variable is not present in the environment.
 
 # 2 Commandline 
 $ pip install "python-dotenv[cli]"
@@ -45,11 +41,13 @@ $ dotenv --help
 
 # 3 Dotenv Values
 from dotenv import dotenv_values
-config = dotenv_values(".env")  
-# config = {"DOMAIN": "example.org", "ADMIN_EMAIL": "admin@example.org", "ROOT_URL": "example.org/app"}
+config = dotenv_values(".env")
+config_secrets = dotenv_values(".secrets")
+print(config, config_secrets["USER"])
 
 # 4 This notably enables advanced configuration management
 config = {
+    **dotenv_values(".env"),
     **dotenv_values(".env.shared"),  # load shared development variables
     **dotenv_values(".env.secret"),  # load sensitive variables
     **os.environ,  # override loaded values with environment variables
@@ -59,14 +57,13 @@ config = {
 # Load from other sources then Filesystem
 from io import StringIO
 config = StringIO("USER=foo\nEMAIL=foo@example.org")
-config = "environment varaibles from your backend"
+config = "environment varaibles from your backend/ server/ cloud"
 load_dotenv(stream=config)
 
-
-# Override
+# 6 Override
 # it looks in both, the env file and the environment, if the variable exists in both directions
 load_dotenv(override=True)
-# prefers the env
+# prefers the .env
 
 load_dotenv(override=False)
 # prefers the environment
@@ -75,5 +72,5 @@ load_dotenv(override=False)
 %load_ext dotenv
 %dotenv
 
-or specify a path
+# or specify a path
 %dotenv relative/or/absolute/path/to/.env
